08fd6d018d3c135fad317ade54df73bae7511252
Herland Mojica <mojicacardozoh@gmail.com>
2024-03-01 09:32:15 -0400

    ejercicio 2

--- src/Ejercicios/Examen.java
+++ src/Ejercicios/Examen.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class Examen {
     public static void main(String[] args) {
-       cargarManual(5);
+        int[][] matriz = new int[3][3];
+       //cargarManual(5);
+        cargarMan(matriz);
+        mostrarMatriz(matriz);
+        System.out.println("La suma de las orillas es: " + sumarOrillas(matriz));
     }
     //Ejercicio1
     public static void cargarManual(int n) {
@@ -27,5 +31,37 @@ public class Examen {
         }
         vectorForm.append("]");
         System.out.println(vectorForm.toString());
+    }
+
+    /*Matriz*/
+    public static void cargarMan(int[][] matriz) {
+        Scanner datoEntrada = new Scanner(System.in);
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.print("Ingrese valor: ");
+                matriz[fila][columna] = datoEntrada.nextInt();
+            }
+        }
+    }
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int fila = 0; fila < matriz.length; fila++) {
+            System.out.print("|");
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.print(" " + matriz[fila][columna] + " ");
+            }
+            System.out.println("|");
+        }
+    }
+    //Ejercicio 2. Sumar orillas de una matriz y retornar la suma
+    public static int sumarOrillas(int[][] matriz) {
+        int suma = 0;
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                if (fila == 0 || fila == matriz.length - 1 || columna == 0 || columna == matriz[fila].length - 1) {
+                    suma = suma + matriz[fila][columna];
+                }
+            }
+        }
+        return suma;
     }
 }
