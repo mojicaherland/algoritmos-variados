32684503553a552ec3b827b64ffec909b3836936
Herland Mojica <mojicacardozoh@gmail.com>
2024-03-01 09:49:02 -0400

    ejercicio 3

--- src/Ejercicios/Examen.java
+++ src/Ejercicios/Examen.java
@@ -9,6 +9,7 @@ public class Examen {
         cargarMan(matriz);
         mostrarMatriz(matriz);
         System.out.println("La suma de las orillas es: " + sumarOrillas(matriz));
+        System.out.println("¿Hay un ganador? " + tresEnRaya(matriz));
     }
     //Ejercicio1
     public static void cargarManual(int n) {
@@ -63,5 +64,27 @@ public class Examen {
             }
         }
         return suma;
+    }
+    //Ejercicio 3. Dado 3 en raya, verificar si hay un ganador
+    public static boolean tresEnRaya(int[][] matriz) {
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                if (matriz[fila][columna] == 1) {
+                    if (columna + 2 < matriz[fila].length && matriz[fila][columna + 1] == 1 && matriz[fila][columna + 2] == 1) {
+                        return true;
+                    }
+                    if (fila + 2 < matriz.length && matriz[fila + 1][columna] == 1 && matriz[fila + 2][columna] == 1) {
+                        return true;
+                    }
+                    if (fila + 2 < matriz.length && columna + 2 < matriz[fila].length && matriz[fila + 1][columna + 1] == 1 && matriz[fila + 2][columna + 2] == 1) {
+                        return true;
+                    }
+                    if (fila + 2 < matriz.length && columna - 2 >= 0 && matriz[fila + 1][columna - 1] == 1 && matriz[fila + 2][columna - 2] == 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
